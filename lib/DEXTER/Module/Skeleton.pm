364239618008bf3e28ca359b003877798498bd46
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-26 16:06:13 +0100

    Compatible with 5.8

--- lib/DEXTER/Module/Skeleton.pm
+++ lib/DEXTER/Module/Skeleton.pm
@@ -18,7 +18,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.0100_20131230';
+our $VERSION = '0.0100_20150326';
 
 
 =head1 METHODS
@@ -135,7 +135,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2012-2013 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2012-2013, 2015 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
